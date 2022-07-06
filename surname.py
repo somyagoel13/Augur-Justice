b9e5f7a8aa11c61805a3fe29ca2aae10c15a2fb4
Somya Goel <37614599+somyagoel13@users.noreply.github.com>
2022-07-06 21:16:12 +0530

    Add files via upload

--- surname.py
+++ surname.py
@@ -12,13 +12,13 @@ def binarySearch (arr, l, r, x):
             return binarySearch(arr, mid+1, r, x) 
     else: 
         return -1
-def detect_religion:
-    
 file1=open("muslim surname.txt")
 file2=open("hindu surname.txt")
 file3=open("christian surname.txt")
 x =input("Enter your surname   ")
+y=input("Enter your religion   ")
 x=x.capitalize()
+y=y.capitalize()
 a=file1.read()
 b=file2.read()
 c=file3.read()
@@ -32,7 +32,10 @@ p=word_tokenize(c)
 file1.close()
 file2.close()
 file3.close()
-if binarySearch(n, 0, len(n)-1, x)!= -1 or y=="Muslim": 
+resultm = binarySearch(n, 0, len(n)-1, x)
+resulth = binarySearch(m, 0, len(m)-1, x)
+resultc = binarySearch(p, 0, len(p)-1, x)
+if resultm!= -1 or y=="Muslim": 
     f1=open("muslim.txt")
     f2=open("muslim01.txt")
     l1=f1.read()
@@ -43,7 +46,8 @@ if binarySearch(n, 0, len(n)-1, x)!= -1 or y=="Muslim":
         li1.append(tuple((arr[i],arr2[i])))
     f1.close()
     f2.close()
-elif binarySearch(m, 0, len(m)-1, x)!=-1 or y=="Hindu": 
+    print("Also see: Dissolution of Musim Marriage")
+elif resulth!=-1 or y=="Hindu": 
     f1=open("hindu.txt")
     f2=open("hindu01.txt")
     l1=f1.read()
@@ -52,15 +56,10 @@ elif binarySearch(m, 0, len(m)-1, x)!=-1 or y=="Hindu":
     arr2=word_tokenize(l2)
     for i in range(0,len(arr)):
         li1.append(tuple((arr[i],arr2[i])))
-    for i in range(40,100,10):
-        length=(i/100)*len(li1)
-        traning_set=li1[:length]
-        testing_set=li1[length:]
-        model= NaiveBayesClassifier(training_set)
-        print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)        
     f1.close()
     f2.close()
-elif binarySearch(p, 0, len(p)-1, x)!=-1 or y=="Christian":
+    print("Also see: Hindu Marriage Act")
+elif resultc!=-1 or y=="Christian":
     f1=open("Christian.txt")
     f2=open("christian01.txt")
     l1=f1.read()
@@ -71,17 +70,17 @@ elif binarySearch(p, 0, len(p)-1, x)!=-1 or y=="Christian":
         li1.append(tuple((arr[i],arr2[i])))
     f1.close()
     f2.close()
-    
-#mycase=input("Enter your case")
+    print("Also see: Indian Divorce Act")
+mycase=input("Enter your case")
 c1=0
 c2=0
 model= NaiveBayesClassifier(li1)
-"""print(model.classify(mycase))
+#print(model.classify(mycase))
 case=sent_tokenize(mycase)
 for i in range(0,len(case)):
     temp=model.classify(case[i])
-    if temp=="0":
+    if temp=="0" or temp==0:
         c1=c1+1
     else:
         c2=c2+1
-print("Probability of winning case",c1/(c1+c2))"""
+print("Probability of winning case",c1/(c1+c2))
